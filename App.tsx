7c514841ce615f38b70e2970d35a97dd4eaef591
OlenaHlu <glushenkova_es@ukr.net>
2025-02-19 18:48:47 +0200

    add basic components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Text>Hello world</Text>
+      <View>
+        <Text>Раді тебе вітати!</Text>
+        <Text>
+          Кожен пухнастик заслуговує на дбайливих господарів.Ми допоможемо тобі
+          знайти друга.
+        </Text>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Вхід</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Реєстрація</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <View>
+          <TextInput />
+        </View>
+        <View>
+          <TextInput />
+        </View>
+      </View>
+      <TouchableOpacity>
+        <Text>Увійти</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
